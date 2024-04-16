647d5b5ce88e27784f2510cb1728ee7840f1806b
axpoty <axel.poty@le-campus-numerique.fr>
2024-04-16 09:53:10 +0200

    feat: add tests for throughDirectoryLike

--- src/test/utils.test.ts
+++ src/test/utils.test.ts
@@ -1,8 +1,8 @@
 import path from "path";
 import fs from 'fs'
-import {throughDirectory} from "../routes/utils";
+import {throughDirectory, throughDirectoryLike} from "../routes/utils";
 
-describe('ThroughDirectory function', () => {
+describe('throughDirectory function', () => {
     const testFolderPath = path.join(__dirname, 'testFolder');
 
     // Mocking fs.readdirSync and fs.statSync
@@ -61,6 +61,70 @@ describe('ThroughDirectory function', () => {
             .mockReturnValueOnce({isDirectory: () => false, size: 100})
 
         const result = throughDirectory(testFolderPath);
+        expect(result).toEqual(expectedFiles);
+    });
+});
+
+
+describe('throughDirectoryLike function', () => {
+    const testFolderPath = path.join(__dirname, 'testFolder');
+
+    // Mocking fs.readdirSync and fs.statSync
+    jest.mock('fs');
+
+    interface MockStats {
+        isDirectory: () => boolean
+        size?: number
+    }
+
+    const readdirSync = jest.spyOn(fs, 'readdirSync') as unknown as jest.SpyInstance<string[]>;
+    const statSync = jest.spyOn(fs, 'statSync') as unknown as jest.SpyInstance<MockStats>;
+
+    beforeEach(() => {
+        jest.resetAllMocks();
+    });
+
+    it('should return an array of files in the directory', () => {
+        const testFiles = ['file1.txt', 'file2.txt'];
+        const expectedFiles = [
+            {name: path.join(testFolderPath, 'file1.txt'), isFolder: false, size: 100},
+            {name: path.join(testFolderPath, 'file2.txt'), isFolder: false, size: 150},
+        ];
+
+        // Mocking fs.readdirSync to return testFiles
+
+        readdirSync.mockReturnValue(testFiles);
+
+        // Mocking fs.statSync to return appropriate stats for each file
+        statSync
+            .mockReturnValueOnce({isDirectory: () => false, size: 100})
+            .mockReturnValueOnce({isDirectory: () => false, size: 150});
+
+        const result = throughDirectoryLike(/file/i, testFolderPath);
+        expect(result).toEqual(expectedFiles);
+    });
+
+    it('should handle nested directories', () => {
+        const testFiles = ['testFile1.txt', 'subTestFolder'];
+        const testFiles2 = ['subTestFile.txt'];
+        const expectedFiles = [
+            {name: path.join(testFolderPath, 'testFile1.txt'), isFolder: false, size: 100},
+            {name: path.join(testFolderPath, 'subTestFolder'), isFolder: true, size: undefined},
+            {name: path.join(testFolderPath, 'subTestFolder/subTestFile.txt'), isFolder: false, size: 100},
+        ];
+
+        // Mocking fs.readdirSync to return testFiles
+        readdirSync
+            .mockReturnValueOnce(testFiles)
+            .mockReturnValueOnce(testFiles2);
+
+        // Mocking fs.statSync to return appropriate stats for each file
+        statSync
+            .mockReturnValueOnce({isDirectory: () => false, size: 100})
+            .mockReturnValueOnce({isDirectory: () => true})
+            .mockReturnValueOnce({isDirectory: () => false, size: 100})
+
+        const result = throughDirectoryLike(/test/i, testFolderPath);
         expect(result).toEqual(expectedFiles);
     });
 });
