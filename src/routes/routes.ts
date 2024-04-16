301363856582a67cdd014aaaea3842660f999b18
axpoty <axel.poty@le-campus-numerique.fr>
2024-04-16 16:08:26 +0200

    refactor: remove redundant routes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -3,18 +3,12 @@ import {getFolder, postFolder, putFile, deleteFile} from "./app";
 
 const router = express.Router();
 
+router.get('/?*', getFolder)
 
-router.get('/', getFolder)
+router.post('/?*', postFolder)
 
-router.get('/*', getFolder)
+router.put('/?*', putFile)
 
-router.post('/*', postFolder)
-router.post('/', postFolder)
-
-router.put('/*', putFile)
-router.put('/', putFile)
-
-router.delete('/*/:name', deleteFile)
-router.delete('/:name', deleteFile)
+router.delete('/?*/:name', deleteFile)
 
 export default router;
